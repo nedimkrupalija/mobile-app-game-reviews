b45488996ffd5f42c2ad0003c00866d9dda73e7a
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-04-08 18:46:23 +0200

    added menu bar

--- app/src/main/java/ba/unsa/etf/rma/spirala/HomeActivity.kt
+++ app/src/main/java/ba/unsa/etf/rma/spirala/HomeActivity.kt
@@ -1,58 +1,22 @@
 package ba.unsa.etf.rma.spirala
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import ba.unsa.etf.rma.spirala.GameData.Companion.getAll
-import ba.unsa.etf.rma.spirala.GameData.Companion.getDetails
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var games: RecyclerView
-    private lateinit var gameListAdapter: GameListAdapter
-    private var gamesList = getAll()
-    private lateinit var homeButton: Button
-    private lateinit var detailsButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-       games = findViewById(R.id.game_list)
-        games.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
-        gameListAdapter = GameListAdapter(arrayListOf()) {game -> showDetails(game)}
-        games.adapter = gameListAdapter
-        gameListAdapter.updateGames(gamesList)
 
-        homeButton = findViewById(R.id.home_button)
-        detailsButton = findViewById(R.id.details_button)
-        homeButton.isEnabled = false
-
-        val extras = intent.extras
-        detailsButton.isEnabled = extras != null
-
-        detailsButton.setOnClickListener{
-            if(extras!=null){
-                val game = getDetails(extras.getString("game_title",""))
-                showDetails(game!!)
-            }
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        navView.setupWithNavController(navController)
     }
-    private fun showDetails(game: Game){
-        val intent = Intent(this, GameDetailsActivity::class.java).apply{
-            putExtra("game_title",game.title)
-        }
-        startActivity(intent)
-    }
-
-
-
-
 }
